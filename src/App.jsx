7a14c1494a2bbf839596bcfa37277f9b3f39826f
Janudi Adhikari <156613694+JanudiAdhikari@users.noreply.github.com>
2026-04-13 12:29:27 +0530

    initialize React app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen">
+        <h1>Ceylon Pepper Web Initialized</h1>
+      </div>
+    </Router>
+  )
+}
+
+export default App
